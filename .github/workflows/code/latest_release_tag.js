af118f5abe964cac5b4fdfe91c5866f4978124c8
allardy <yann.allard1@gmail.com>
2021-09-09 19:15:15 -0400

    test build

---
+++ .github/workflows/code/latest_release_tag.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process')
+
+/**
+ * Returns the latest release tag (ignore branches)
+ */
+const getLastTags = async () => {
+  exec('git rev-list --tags --max-count=20', (err, revsList, stderr) => {
+    const revisions = revsList.trim().split('\n')
+
+    exec(`git describe --abbrev=0 --tags ${revisions.join(' ')}`, (err, result) => {
+      const tags = result.trim().split('\n')
+
+      for (i = 0; i < revisions.length; i++) {
+        if (/^v\d/.test(tags[i])) {
+          return console.log(`::set-output name=tag::${tags[i]}`)
+        }
+      }
+    })
+  })
+}
+
+getLastTags()
