aab5cadc34603b6a223f8fb572a0ade0d13f66f4
allardy <yann.allard1@gmail.com>
2021-09-09 21:35:50 -0400

    test

--- .github/workflows/code/latest_release_tag.js
+++ .github/workflows/code/latest_release_tag.js
@@ -6,11 +6,14 @@ require('bluebird-global')
  * Returns the latest release tag (ignore branches)
  */
 const getLastTags = async () => {
+  console.log('Getlast')
   const rawTags = await Promise.fromCallback(cb => exec('git rev-list --tags --max-count=20', cb))
   const tags = rawTags
     .trim()
     .split('\n')
     .join(' ')
+
+  console.log('TAGS', rawTags, tags)
 
   const rawRevs = await Promise.fromCallback(cb => exec(`git describe --abbrev=0 --tags ${tags}`, cb))
   const revs = rawRevs.trim().split('\n')
