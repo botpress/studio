f5f59c3dd939c5026c0b9902d5e39c9d8ca232a1
allardy <yann.allard1@gmail.com>
2021-09-09 22:44:53 -0400

    test

--- .github/workflows/code/latest_release_tag/index.js
+++ .github/workflows/code/latest_release_tag/index.js
@@ -9,12 +9,22 @@ exec('git rev-list --tags --max-count=20', (err, rawTags, stderr) => {
   console.log(`::set-output name=bla::hello`)
   core.setOutput('test2', 'blab')
 
+  if (err) {
+    console.log('err rev', stderr)
+    process.exit(1)
+  }
+
   const tags = rawTags
     .trim()
     .split('\n')
     .join(' ')
 
   exec(`git describe --abbrev=0 --tags ${tags}`, (err, rawRevs, stderr) => {
+    if (err) {
+      console.log('err desc', stderr)
+      process.exit(1)
+    }
+
     console.log('revs', rawRevs)
     const revs = rawRevs.trim().split('\n')
 
