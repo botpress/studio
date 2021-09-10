afaeee31b50645c89545526bc80ed13bc012d774
allardy <yann.allard1@gmail.com>
2021-09-09 21:42:28 -0400

    test

--- .github/workflows/code/latest_release_tag.js
+++ .github/workflows/code/latest_release_tag.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process')
-const core = require('@actions/core')
+// const core = require('@actions/core')
 require('bluebird-global')
 
 /**
@@ -7,6 +7,9 @@ require('bluebird-global')
  */
 const getLastTags = async () => {
   console.log('Getlast')
+
+  const status = await Promise.fromCallback(cb => exec('git status', cb))
+  console.log('status', status)
   const rawTags = await Promise.fromCallback(cb => exec('git rev-list --tags --max-count=20', cb))
   const tags = rawTags
     .trim()
@@ -20,7 +23,7 @@ const getLastTags = async () => {
 
   for (i = 0; i < revs.length; i++) {
     if (/^v\d/.test(revs[i])) {
-      core.setOutput('LAST_TAG', revs[i])
+      //core.setOutput('LAST_TAG', revs[i])
       console.log(revs[i])
       return revs[i]
     }
