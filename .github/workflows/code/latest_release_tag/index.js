089b9466b06d7b12f363db187f67f46390a61643
allardy <yann.allard1@gmail.com>
2021-09-09 22:41:45 -0400

    test

--- .github/workflows/code/latest_release_tag/index.js
+++ .github/workflows/code/latest_release_tag/index.js
@@ -1,26 +1,29 @@
 const { exec } = require('child_process')
 const core = require('@actions/core')
 
-const getLastTags = async () => {
-  core.setOutput('test', 'bla')
-  exec('git rev-list --tags --max-count=20', (err, rawTags, stderr) => {
-    const tags = rawTags
-      .trim()
-      .split('\n')
-      .join(' ')
+console.log('gettags')
+core.setOutput('test', 'bla')
 
-    exec(`git describe --abbrev=0 --tags ${tags}`, (err, rawRevs, stderr) => {
-      const revs = rawRevs.trim().split('\n')
+exec('git rev-list --tags --max-count=20', (err, rawTags, stderr) => {
+  console.log('list', rawTags)
+  console.log(`::set-output name=bla::hello`)
+  core.setOutput('test2', 'blab')
 
-      for (i = 0; i < revs.length; i++) {
-        if (/^v\d/.test(revs[i])) {
-          core.setOutput('tag', revs[i])
-          console.log(revs[i])
-          return revs[i]
-        }
+  const tags = rawTags
+    .trim()
+    .split('\n')
+    .join(' ')
+
+  exec(`git describe --abbrev=0 --tags ${tags}`, (err, rawRevs, stderr) => {
+    console.log('revs', rawRevs)
+    const revs = rawRevs.trim().split('\n')
+
+    for (i = 0; i < revs.length; i++) {
+      if (/^v\d/.test(revs[i])) {
+        core.setOutput('tag', revs[i])
+        console.log(revs[i])
+        return revs[i]
       }
-    })
+    }
   })
-}
-
-getLastTags()
+})
