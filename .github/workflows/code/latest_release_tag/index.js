9e31460d743b4c10d4949399bcb895204122a8f5
allardy <yann.allard1@gmail.com>
2021-09-09 22:51:07 -0400

    test

--- .github/workflows/code/latest_release_tag/index.js
+++ .github/workflows/code/latest_release_tag/index.js
@@ -1,39 +1,25 @@
 const { exec } = require('child_process')
-const core = require('@actions/core')
+// const core = require('@actions/core')
+require('bluebird-global')
 
-console.log('gettags')
-core.setOutput('test', 'bla')
-
-exec('git rev-list --tags --max-count=20', (err, rawTags, stderr) => {
-  console.log('list', rawTags)
-  console.log(`::set-output name=bla::hello`)
-  core.setOutput('test2', 'blab')
-
-  if (err) {
-    console.log('err rev', stderr)
-    // process.exit(1)
-  }
-
+const getLastTags = async () => {
+  const rawTags = await Promise.fromCallback(cb => exec('git rev-list --tags --max-count=20', cb))
   const tags = rawTags
     .trim()
     .split('\n')
     .join(' ')
 
-  exec(`git describe --abbrev=0 --tags ${tags}`, (err, rawRevs, stderr) => {
-    if (err) {
-      console.log('err desc', stderr)
-      // process.exit(1)
-    }
+  const rawRevs = await Promise.fromCallback(cb => exec(`git describe --abbrev=0 --tags ${tags}`, cb))
+  const revs = rawRevs.trim().split('\n')
 
-    console.log('revs', rawRevs)
-    const revs = rawRevs.trim().split('\n')
-
-    for (i = 0; i < revs.length; i++) {
-      if (/^v\d/.test(revs[i])) {
-        core.setOutput('tag', revs[i])
-        console.log(revs[i])
-        return revs[i]
-      }
+  for (i = 0; i < revs.length; i++) {
+    if (/^v\d/.test(revs[i])) {
+      // core.setOutput('last_tag', revs[i])
+      // console.log(revs[i])
+      console.log(`::set-output name=tag::${revs[i]}`)
+      return revs[i]
     }
-  })
-})
+  }
+}
+
+getLastTags()
