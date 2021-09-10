99e2516dc62fc12ef7b7fe67d28475253b6b2cba
allardy <yann.allard1@gmail.com>
2021-09-09 22:32:48 -0400

    test

--- .github/workflows/code/latest_release/index.js
+++ .github/workflows/code/latest_release/index.js
@@ -1,38 +1,28 @@
 const { exec } = require('child_process')
 const core = require('@actions/core')
-require('bluebird-global')
 
 /**
  * Returns the latest release tag (ignore branches)
  */
 const getLastTags = async () => {
-  console.log('getLastTags')
+  exec('git rev-list --tags --max-count=20', (err, rawTags, stderr) => {
+    const tags = rawTags
+      .trim()
+      .split('\n')
+      .join(' ')
 
-  // const status = await Promise.fromCallback(cb => exec('git status', cb))
+    exec(`git describe --abbrev=0 --tags ${tags}`, (err, rawRevs, stderr) => {
+      const revs = rawRevs.trim().split('\n')
 
-  // exec('git rev-list --tags', (err, revsList, stderr) => {
-  //   console.log('RVLIST', revsList)
-  // })
-
-  // console.log('status', status)
-  const rawTags = await Promise.fromCallback(cb => exec('git rev-list --tags --max-count=20', cb))
-  const tags = rawTags
-    .trim()
-    .split('\n')
-    .join(' ')
-
-  console.log('TAGS', rawTags, tags)
-
-  const rawRevs = await Promise.fromCallback(cb => exec(`git describe --abbrev=0 --tags ${tags}`, cb))
-  const revs = rawRevs.trim().split('\n')
-
-  for (i = 0; i < revs.length; i++) {
-    if (/^v\d/.test(revs[i])) {
-      core.setOutput('last_tag', revs[i])
-      console.log(revs[i])
-      return revs[i]
-    }
-  }
+      for (i = 0; i < revs.length; i++) {
+        if (/^v\d/.test(revs[i])) {
+          core.setOutput('last_tag', revs[i])
+          console.log(revs[i])
+          return revs[i]
+        }
+      }
+    })
+  })
 }
 
 getLastTags()
