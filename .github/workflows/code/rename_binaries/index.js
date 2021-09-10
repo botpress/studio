e6d3152aa515049b91c9ecd115ec368f97fdc2c7
allardy <yann.allard1@gmail.com>
2021-09-10 11:08:43 -0400

    tst

--- .github/workflows/code/rename_binaries/index.js
+++ .github/workflows/code/rename_binaries/index.js
@@ -3,11 +3,15 @@ const path = require('path')
 
 const sanitizeBranchName = input => input.replace('refs/heads/', '').replace(/[\W_]+/g, '_')
 
+console.log('d', process.env.GITHUB_REF)
 const BIN_PATH = process.env.INPUT_PATH
+
+console.log(process.env)
+console.log('bp', BIN_PATH)
+
 const githubEvent = JSON.parse(process.env.GITHUB_EVENT)
 const branchName = sanitizeBranchName(githubEvent.ref)
-
-console.log(BIN_PATH, branchName)
+console.log('branch', branchName)
 for (const fileName of fs.readdirSync(BIN_PATH)) {
   const [name, _version, platform, arch] = fileName.split('-')
   const newName = `${name}-${branchName}-${platform}-${arch}`
