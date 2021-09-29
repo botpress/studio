5429f4c2a8b886b6ec41e58e6af1d0656fbf7a2a
François Levasseur <francois_levasseur@hotmail.com>
2021-09-29 19:26:48 -0400

    chore(dx): added a default env file used in launch.json file for debugging (#124)
    * chore(dx): added a default env file used in launch.json file for debugging
    
    * add more defaults variables to the env config
    
    * Update scripts/gulp.env.debug.js
    
    Co-authored-by: Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
    
    * pr review comments
    
    Co-authored-by: Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>

---
+++ scripts/gulp.env.debug.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const minimist = require('minimist')
+
+const DEFAULT_INTERNAL_PASSWORD = '$YOUR_INTERNAL_PASSWORD'
+const DEFAULT_BP_PATH = '$YOUR_BP_PATH'
+const DEFAULT_ENV_VALUE = `
+# use this env file to modify env variables for debug purposes (see launch.json for more details)
+
+BP_DATA_FOLDER=${DEFAULT_BP_PATH}/packages/bp/dist/data
+BP_MODULES_PATH=${DEFAULT_BP_PATH}/modules
+NLU_ENDPOINT=http://localhost:3200
+STUDIO_PORT=4000
+
+CORE_PORT=3000
+ROOT_PATH=''
+INTERNAL_PASSWORD=${DEFAULT_INTERNAL_PASSWORD}
+`
+
+const createDefaultEnvFileForDebugging = cb => {
+  const { location } = minimist(process.argv.slice(2))
+  if (!location) {
+    return cb(
+      new Error('Creating empty env file for debugging requires the desired file location as 1st positional parameter.')
+    )
+  }
+
+  if (fs.existsSync(location)) {
+    return cb()
+  }
+
+  fs.writeFileSync(location, DEFAULT_ENV_VALUE)
+  return cb()
+}
+
+module.exports = {
+  createDefaultEnvFileForDebugging
+}
