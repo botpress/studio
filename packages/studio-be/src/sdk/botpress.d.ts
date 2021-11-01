2582acf94010e7c468106cacc3641dc1c95933aa
Sylvain Perron <sylvain.perron@botpress.com>
2021-11-01 18:49:44 -0400

    fix styling

--- packages/studio-be/src/sdk/botpress.d.ts
+++ packages/studio-be/src/sdk/botpress.d.ts
@@ -539,7 +539,6 @@ declare module 'botpress/sdk' {
     export interface EventUnderstanding {
       readonly errored: boolean
       readonly modelId: string | undefined
-      
       readonly predictions?: {
         [context: string]: {
           confidence: number
