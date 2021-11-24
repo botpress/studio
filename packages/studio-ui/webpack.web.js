0834907f998f6b1ace34fa0fc70850cc9c280b27
allardy <yann.allard1@gmail.com>
2021-11-24 16:37:56 -0500

    chore(dx): better build experience

--- packages/studio-ui/webpack.web.js
+++ packages/studio-ui/webpack.web.js
@@ -3,9 +3,8 @@ process.traceDeprecation = true
 const chalk = require('chalk')
 const webpack = require('webpack')
 const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
@@ -77,21 +76,42 @@ const webConfig = {
         NODE_ENV: isProduction ? JSON.stringify('production') : JSON.stringify('development')
       }
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, './src/web/img'),
-        to: path.resolve(__dirname, './public/img')
-      },
-      {
-        from: path.resolve(__dirname, './src/web/audio'),
-        to: path.resolve(__dirname, './public/audio')
-      },
-      {
-        from: path.resolve(__dirname, './src/web/external'),
-        to: path.resolve(__dirname, './public/external')
-      }
-    ]),
-    new CleanWebpackPlugin(['public'])
+    new FileManagerPlugin({
+      onStart: [
+        {
+          delete: ['public']
+        },
+        {
+          copy: [
+            {
+              source: path.resolve(__dirname, './src/web/img'),
+              destination: path.resolve(__dirname, './public/img')
+            },
+            {
+              source: path.resolve(__dirname, './src/web/audio'),
+              destination: path.resolve(__dirname, './public/audio')
+            },
+            {
+              source: path.resolve(__dirname, './src/web/external'),
+              destination: path.resolve(__dirname, './public/external')
+            }
+          ]
+        }
+      ],
+      onEnd: [
+        {
+          delete: [path.resolve(__dirname, '../studio-be/out/ui/public')]
+        },
+        {
+          copy: [
+            {
+              source: 'public',
+              destination: path.resolve(__dirname, '../studio-be/out/ui/public')
+            }
+          ]
+        }
+      ]
+    })
   ],
 
   module: {
