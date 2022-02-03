e17177fc04e1600402b7d029d39b24c617119e85
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-02-03 18:03:29 -0500

    remove coverage comment

--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -3,15 +3,6 @@ import { defaults as tsjPreset } from 'ts-jest/presets'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  // TODO: Re-enable coverage threshold once we have enough tests
-  /*coverageThreshold: {
-    global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60
-    }
-  }*/
   projects: [
     {
       rootDir: 'packages/studio-be/src',
