205c0f50adcd273b6d9227df5548347aaaf47eca
Samuel Massé <samuelmasse4@gmail.com>
2022-02-07 16:47:43 -0500

    fix eslint

--- packages/studio-ui/src/web/reducers/ui.ts
+++ packages/studio-ui/src/web/reducers/ui.ts
@@ -70,7 +70,7 @@ const reducer = handleActions(
       ...state,
       docModal: payload
     }),
-    [toggleBottomPanelExpand]: state => ({
+    [toggleBottomPanelExpand]: (state) => ({
       ...state,
       bottomPanelExpanded: !state.bottomPanelExpanded
     }),
