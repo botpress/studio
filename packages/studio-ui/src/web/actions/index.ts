33269e6e0d6fd8a00c79b2b333c48795f7c6f4bb
Sylvain Perron <sylvain.perron@botpress.com>
2022-02-08 16:53:27 -0500

    re-added channel-web

--- packages/studio-ui/src/web/actions/index.ts
+++ packages/studio-ui/src/web/actions/index.ts
@@ -359,7 +359,20 @@ export const fetchBotInformation = () => (dispatch) => {
 export const modulesReceived = createAction('MODULES/RECEIVED')
 export const fetchModules = () => (dispatch) => {
   // TODO cleanup
-  dispatch(modulesReceived([]))
+  dispatch(
+    modulesReceived([
+      {
+        fullName: 'Web Chat',
+        menuIcon: 'view_module',
+        menuText: 'channel-web',
+        moduleView: { stretched: false },
+        noInterface: true,
+        plugins: [{ entry: 'WebBotpressUIInjection', position: 'overlay' }],
+        name: 'channel-web',
+        homepage: 'https://botpress.com'
+      }
+    ])
+  )
 }
 
 // Skills
