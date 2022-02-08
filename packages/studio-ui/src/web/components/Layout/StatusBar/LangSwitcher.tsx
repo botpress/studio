4b7985c3ed3f983bc28537a32d694a469073548b
Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
2022-02-08 13:31:55 -0500

    chore(layout): use bp3 dropdown for lang switcher (#282)
    * chore(layout): use bp3 dropdown for lang switcher
    
    * get rid of function

--- packages/studio-ui/src/web/components/Layout/StatusBar/LangSwitcher.tsx
+++ packages/studio-ui/src/web/components/Layout/StatusBar/LangSwitcher.tsx
@@ -1,24 +1,15 @@
-import { lang, utils } from 'botpress/shared'
+import { Button } from '@blueprintjs/core'
+import { Dropdown } from 'botpress/shared'
 import _ from 'lodash'
-import React, { Fragment } from 'react'
-import { Dropdown } from 'react-bootstrap'
+import React, { useEffect } from 'react'
 
 import withLanguage from '../../Util/withLanguage'
 
-import ActionItem from './ActionItem'
-import style from './style.scss'
-
-const requireFlag = (code) => {
-  try {
-    return require(`../../../img/flags/${code}.svg`)
-  } catch (err) {
-    return requireFlag('missing')
-  }
-}
+import { Flag } from './Flag'
 
 interface Props {
   contentLang: string
-  languages: any
+  languages: string[]
   changeContentLanguage: any
   toggleLangSwitcher: any
   langSwitcherOpen: boolean
@@ -26,97 +17,45 @@ interface Props {
 
 const STORAGE_KEY = `bp::${window.BOT_ID}::cmsLanguage`
 
-class LangSwitcher extends React.Component<Props> {
-  elems = {}
-
-  componentDidMount() {
-    this.restoreLastLanguage()
-  }
-
-  componentDidUpdate(prevProps) {
-    const idx = this.props.languages.findIndex((l) => l === this.props.contentLang)
-    if (idx !== -1 && !_.isEmpty(this.elems)) {
-      this.elems[idx].focus()
-    }
-
-    if (prevProps.languages !== this.props.languages) {
-      this.restoreLastLanguage()
-    }
-  }
-
-  restoreLastLanguage() {
+export const LanguageSwitcher = (props: Props) => {
+  useEffect(() => {
     const lastLang = localStorage.getItem(STORAGE_KEY)
-    if (!this.props.languages || !this.props.languages.length || !lastLang) {
+    if (!props.languages || !props.languages.length || !lastLang) {
       return
     }
 
-    if (this.props.languages.includes(lastLang)) {
-      this.props.changeContentLanguage(lastLang)
+    if (props.languages.includes(lastLang)) {
+      props.changeContentLanguage(lastLang)
     }
-  }
+  }, [props.languages])
 
-  handleKeyDown = (l, e) => {
-    if (e.key === 'Enter') {
-      this.switchLang(l)
-    }
-  }
-
-  switchLang = (lang) => {
-    this.props.changeContentLanguage(lang)
-    this.props.toggleLangSwitcher()
+  const switchLang = (lang: string) => {
+    props.changeContentLanguage(lang)
+    props.toggleLangSwitcher()
 
     localStorage.setItem(STORAGE_KEY, lang)
   }
 
-  // react-bootstrap warning otherwise
-  onToggle() {}
+  const items = props.languages.map((langCode) => ({
+    value: langCode,
+    label: langCode.toUpperCase(),
+    icon: <Flag languageCode={langCode} />
+  }))
 
-  render() {
-    if (this.props.languages.length <= 1) {
-      return null
-    }
-
-    return (
-      <Fragment>
-        <ActionItem
-          shortcut={utils.keyMap['lang-switcher']}
-          title={lang.tr('statusBar.contentLanguage')}
-          description={lang.tr('statusBar.switchLang', { currentLang: this.props.contentLang.toUpperCase() })}
-          onClick={this.props.toggleLangSwitcher}
-        >
-          <span className={style.flagWrapper}>
-            <img src={requireFlag(this.props.contentLang)} alt={this.props.contentLang} className={style.flag} />
-            <span>{this.props.contentLang.toUpperCase()}</span>
-          </span>
-        </ActionItem>
-        <Dropdown
-          pullRight
-          dropup={true}
-          open={this.props.langSwitcherOpen}
-          onToggle={this.onToggle}
-          id="lang-switcher"
-        >
-          {/* react-bootstrap warning otherwise */}
-          <Dropdown.Toggle style={{ display: 'none' }} />
-          <Dropdown.Menu pullRight onClose={this.props.toggleLangSwitcher} className={style.langSwitherMenu}>
-            {this.props.languages.map((l, idx) => (
-              <li
-                tabIndex={-1}
-                ref={(el) => (this.elems[idx] = el)}
-                key={l}
-                className={style.langItem}
-                onClick={this.switchLang.bind(this, l)}
-                onKeyDown={this.handleKeyDown.bind(this, l)}
-              >
-                <img src={requireFlag(l)} alt={l} className={style.flag} />
-                <span>{l.toUpperCase()}</span>
-              </li>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-      </Fragment>
+  return (
+    props.languages.length > 1 && (
+      <Dropdown
+        items={items}
+        filterable={false}
+        defaultItem={items.find((i) => i.value === props.contentLang)}
+        onChange={(item) => {
+          switchLang(item.value)
+        }}
+      >
+        <Button minimal icon={<Flag languageCode={props.contentLang} />} text={props.contentLang.toUpperCase()} />
+      </Dropdown>
     )
-  }
+  )
 }
 
-export default withLanguage(LangSwitcher)
+export default withLanguage(LanguageSwitcher)
