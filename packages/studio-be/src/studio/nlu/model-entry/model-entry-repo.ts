907f233ff346573946ea239a4018727bd1d46028
Sylvain Perron <sylvain.perron@botpress.com>
2022-02-10 13:52:00 -0500

    added todo

--- packages/studio-be/src/studio/nlu/model-entry/model-entry-repo.ts
+++ packages/studio-be/src/studio/nlu/model-entry/model-entry-repo.ts
@@ -18,13 +18,14 @@ interface ModelEntryRow extends ModelEntryPrimaryKey {
 
 const getKey = (primaryKey: ModelEntryPrimaryKey) => `${primaryKey.botId}/${primaryKey.language}/${primaryKey.status}`
 
+// TODO: Keep this in a file for restarts... right now restarting Botpress marks the model as dirty automatically
 export class ModelEntryRepository {
   private state: { [key: string]: ModelEntryRow } = {}
 
   constructor() {}
 
   public async get(key: ModelEntryPrimaryKey): Promise<ModelEntryRow | undefined> {
-    return { ...this.state[getKey(key)] }
+    return this.state[getKey(key)] ? { ...this.state[getKey(key)] } : undefined
   }
 
   public async set(model: ModelEntryRow) {
