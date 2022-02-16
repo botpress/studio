d9b0dbb7efb33f190c17c77b3867df56442dbd2d
raid55 <boutboulnicholas@gmail.com>
2022-02-16 13:13:34 -0700

    bug fixes

--- packages/superinput/src/EvalPanel.tsx
+++ packages/superinput/src/EvalPanel.tsx
@@ -4,9 +4,7 @@ import { PanelProps } from './types'
 
 const EvalPanel = ({ valid, text }: PanelProps) => {
   return (
-    <div className={`bp-editor-panel ${valid === null ? 'no-eventState' : valid ? 'valid' : 'invalid'}`}>
-      <p>{text}</p>
-    </div>
+    <div className={`bp-editor-panel ${valid === null ? 'no-eventState' : valid ? 'valid' : 'invalid'}`}>{text}</div>
   )
 }
 
