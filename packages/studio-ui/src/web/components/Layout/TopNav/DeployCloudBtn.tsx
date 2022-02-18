655f34faffef7b28ba0e22a482cacbfead50fe37
raid55 <boutboulnicholas@gmail.com>
2022-02-18 02:40:45 -0700

    i18n

--- packages/studio-ui/src/web/components/Layout/TopNav/DeployCloudBtn.tsx
+++ packages/studio-ui/src/web/components/Layout/TopNav/DeployCloudBtn.tsx
@@ -1,9 +1,9 @@
-import { Icon, Tooltip, Spinner, Popover, Position } from '@blueprintjs/core'
+import { Icon, Tooltip, Spinner } from '@blueprintjs/core'
 import axios from 'axios'
-import { CloudConfig } from 'botpress/sdk'
-import { lang, ShortcutLabel, utils, toast } from 'botpress/shared'
+import { lang, toast } from 'botpress/shared'
 import classNames from 'classnames'
 import { Training } from 'common/nlu-training'
+import moment from 'moment'
 import React, { useCallback, useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { RootReducer } from '~/reducers'
@@ -15,27 +15,29 @@ interface Props {
 }
 
 const NeedTraining = () => {
-  return <div className={style.tooltip}>Needs training</div>
+  return <div className={style.tooltip}>{lang.tr('topNav.deploy.tooltip.needsTraining')}</div>
 }
 const DeployInfo = ({ lastImportDate }) => {
   const importDateStr = useCallback(() => {
     if (!lastImportDate) {
       return null
     }
-    const date = new Date(lastImportDate)
-    return date.toString()
+
+    return moment(lastImportDate).fromNow()
   }, [lastImportDate])
 
   return (
-    <div className={style.tooltip}>{importDateStr() ? 'Last Deployed on' + ` ${importDateStr()}` : 'Deploy Bot'}</div>
+    <div className={style.tooltip}>
+      {lastImportDate
+        ? [lang.tr('topNav.deploy.tooltip.lastDeploy'), importDateStr()].join(' ')
+        : lang.tr('topNav.deploy.tooltip.deployBot')}
+    </div>
   )
 }
 const Deploying = () => {
-  return <div className={style.tooltip}>Deploying Now...</div>
+  return <div className={style.tooltip}>{lang.tr('topNav.deploy.tooltip.deploying')}</div>
 }
 
-// archiveUrl: `/admin/workspace/bots/${botId}/export`,
-// archiveName: `bot_${botId}_${Date.now()}.tgz`
 const DeployCloudBtn = (props: Props) => {
   const { trainSession } = props
   const [lastImportDate, setLastImportDate] = useState(null)
@@ -49,24 +51,25 @@ const DeployCloudBtn = (props: Props) => {
         const { imported_on } = data
         setLastImportDate(imported_on)
       })
-      .catch(console.log)
+      .catch((e) => {})
   }, [])
 
   const deployToCloud = useCallback(() => {
-    toast.info('Trying to deploy bot')
+    toast.info(lang.tr('topNav.deploy.toaster.info'))
     setDeployLoading(true)
     axios
       .get(`${window.STUDIO_API_PATH}/cloud/deploy`)
       .then((res) => res.data)
       .then((data) => {
         const { imported_on } = data
-        console.log(data)
-        toast.success('bot uploaded ')
-        setLastImportDate(imported_on)
-        setDeployLoading(false)
+        setTimeout(() => {
+          toast.success(lang.tr('topNav.deploy.toaster.success'))
+          setLastImportDate(imported_on)
+          setDeployLoading(false)
+        }, 10000)
       })
       .catch((e) => {
-        toast.failure('bot not uploaded')
+        toast.failure(lang.tr('topNav.deploy.toaster.error'))
         setDeployLoading(false)
       })
   }, [setLastImportDate, setDeployLoading])
@@ -93,12 +96,12 @@ const DeployCloudBtn = (props: Props) => {
         disabled={deployLoading || !isTrained()}
       >
         {deployLoading ? <Spinner size={16} /> : <Icon icon="cloud-upload" iconSize={16} />}
-        <span className={style.label}>{'Deploy To Cloud'}</span>
+        <span className={style.label}>{lang.tr('topNav.deploy.btn')}</span>
       </button>
     </Tooltip>
   )
 }
-// lang.tr('topNav.emulator')
+
 const mapStateToProps = (state: RootReducer) => ({
   trainSession: state.nlu.trainSessions
 })
