9f7de67047b87504a74ef72399c7d03edc4472f0
Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
2022-02-23 07:44:14 -0500

    fix(content): added translations for card buttons (#304)

--- packages/studio-be/src/builtin/content-types/card.ts
+++ packages/studio-be/src/builtin/content-types/card.ts
@@ -1,7 +1,6 @@
 import { ContentType } from 'botpress/sdk'
 import utils from './_utils'
-import ActionButton from './action_button'
-import Carousel from './carousel'
+import actionButtonSchema from './action_button'
 
 export const cardSchema = {
   description: 'contentTypes.card.description',
@@ -24,8 +23,8 @@ export const cardSchema = {
     },
     actions: {
       type: 'array',
-      title: 'contentTypes.actionButton',
-      items: ActionButton.jsonSchema
+      title: 'contentTypes.card.actionButtons',
+      items: actionButtonSchema
     }
   }
 }
@@ -33,7 +32,7 @@ export const cardSchema = {
 const contentType: ContentType = {
   id: 'builtin_card',
   group: 'Built-in Messages',
-  title: 'card',
+  title: 'contentTypes.card.title',
   jsonSchema: cardSchema,
   uiSchema: {},
 
