697435c400af0906dcd0377d2e4f6aa801281f66
Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
2022-02-22 21:25:55 -0500

    typo

--- packages/studio-ui/src/web/views/FlowBuilder/index.tsx
+++ packages/studio-ui/src/web/views/FlowBuilder/index.tsx
@@ -14,7 +14,7 @@ import {
   setDiagramAction,
   switchFlow
 } from '~/actions'
-import { Container } from '~/components/shared/Interface'
+import { Container } from '~/components/Shared/Interface'
 import { Timeout, toastFailure, toastInfo } from '~/components/Shared/Utils'
 import { isOperationAllowed } from '~/components/Shared/Utils/AccessControl'
 import { RootReducer } from '~/reducers'
