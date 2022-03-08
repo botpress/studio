5b5feeb0d5d5737b1ecfbbe40e56705fc3b8aeb0
raid55 <boutboulnicholas@gmail.com>
2022-03-08 13:59:11 -0700

    refactor functions

--- packages/studio-be/src/studio/cloud/cloud-router.ts
+++ packages/studio-be/src/studio/cloud/cloud-router.ts
@@ -4,114 +4,115 @@ import FormData from 'form-data'
 import qs from 'querystring'
 import { StudioServices } from 'studio/studio-router'
 import { CustomStudioRouter } from 'studio/utils/custom-studio-router'
+import { CloudConfig } from 'botpress/sdk'
 
 export class CloudRouter extends CustomStudioRouter {
   constructor(services: StudioServices) {
     super('Cloud', services)
   }
-  // bots/botBot
+
+  getBearerToken(cloud: CloudConfig) {
+    const { oauthUrl, clientId, clientSecret } = cloud
+
+    return axios
+      .post(
+        oauthUrl,
+        qs.stringify({
+          client_id: clientId,
+          client_secret: clientSecret,
+          grant_type: 'client_credentials'
+        })
+      )
+      .then((res) => `Bearer ${res.data?.access_token}`)
+      .catch((err) => null)
+  }
+
+  getIntrospect(bearerToken: string) {
+    return axios
+      .get(`${process.CONTROLLERAPI_ENDPOINT}/v1/introspect`, {
+        headers: {
+          Authorization: bearerToken
+        }
+      })
+      .then((res) => res.data)
+      .catch((err) => null)
+  }
+
   setupRoutes() {
     this.router.get(
       '/info',
-      this.needPermissions('read', 'bot.content'), // if you can read content you can deploy to cloud...right?
       this.asyncMiddleware(async (req, res) => {
         const botId = req.params.botId
         const { cloud } = (await this.botService.findBotById(botId)) || {}
         if (!cloud) {
           return
         }
-        const { oauthUrl, clientId, clientSecret } = cloud
 
-        const bearerToken = await axios
-          .post(
-            oauthUrl,
-            qs.stringify({
-              client_id: clientId,
-              client_secret: clientSecret,
-              grant_type: 'client_credentials'
-            })
-          )
-          .then((res) => `Bearer ${res.data?.access_token}`)
+        const bearerToken = await this.getBearerToken(cloud)
+        if (!bearerToken) return res.status(401)
 
-        const introspect = await axios
-          .get(`${process.CONTROLLERAPI_ENDPOINT}/v1/introspect`, {
+        const introspect = await this.getIntrospect(bearerToken)
+        if (!introspect) return res.status(404)
+
+        try {
+          const { status, data } = await axios.get(`${process.CONTROLLERAPI_ENDPOINT}/v1/bots/${introspect.botId}`, {
             headers: {
               Authorization: bearerToken
             }
           })
-          .then((res) => res.data)
-
-        const { status, data } = await axios
-          .get(`${process.CONTROLLERAPI_ENDPOINT}/v1/bots/${introspect.botId}`, {
-            headers: {
-              Authorization: bearerToken
-            }
-          })
-          .then((res) => {
-            const { status, data } = res
-            return { status, data }
-          })
-          .catch((e) => {
-            const { status, data } = e
-            return { status, data }
-          })
-        res.status(status).send(data)
+          res.status(status).send(data)
+        } catch (err) {
+          if (err.isAxiosError) {
+            const { status, data } = err.response || { status: 500, data: undefined }
+            res.status(status).send(data)
+          } else {
+            res.status(500)
+          }
+        }
       })
     )
 
     this.router.get(
       '/deploy',
-      this.needPermissions('read', 'bot.content'),
       this.asyncMiddleware(async (req, res) => {
         const botId = req.params.botId
         const { cloud } = (await this.botService.findBotById(botId)) || {}
         if (!cloud) {
           return
         }
-        const { oauthUrl, clientId, clientSecret } = cloud
 
-        const bearerToken = await axios
-          .post(
-            oauthUrl,
-            qs.stringify({
-              client_id: clientId,
-              client_secret: clientSecret,
-              grant_type: 'client_credentials'
-            })
-          )
-          .then((res) => `Bearer ${res.data?.access_token}`)
+        const bearerToken = await this.getBearerToken(cloud)
+        if (!bearerToken) return res.status(401)
+
+        const introspect = await this.getIntrospect(bearerToken)
+        if (!introspect) return res.status(404)
 
         const botBlob = await this.botService.exportBot(botId)
-        const introspect = await axios
-          .get(`${process.CONTROLLERAPI_ENDPOINT}/v1/introspect`, {
-            headers: {
-              Authorization: bearerToken
-            }
-          })
-          .then((res) => res.data)
 
         const botMultipart = new FormData()
         botMultipart.append('botId', introspect.botId)
+        botMultipart.append('botName', introspect.botName)
+        botMultipart.append('botArchive', botBlob, 'bot.tgz')
         botMultipart.append('runtimeName', introspect.runtimeName)
         botMultipart.append('botFileName', `bot_${botId}_${Date.now()}.tgz`)
-        botMultipart.append('botArchive', botBlob, 'bot.tgz')
 
-        const { status, data } = await axios
-          .post(`${process.CONTROLLERAPI_ENDPOINT}/v1/bots/upload`, botMultipart, {
+        try {
+          const { status, data } = await axios.post(`${process.CONTROLLERAPI_ENDPOINT}/v1/bots/upload`, botMultipart, {
             headers: {
               'Content-Type': `multipart/form-data; boundary=${botMultipart.getBoundary()}`,
               Authorization: bearerToken
-            }
+            },
+            maxBodyLength: 100 * 1024 * 1024 // 100 MB
           })
-          .then((res) => {
-            const { status, data } = res
-            return { status, data }
-          })
-          .catch((e) => {
-            const { status, data } = e
-            return { status, data }
-          })
-        res.status(status).send(data)
+          res.status(status).send(data)
+        } catch (err) {
+          if (err.isAxiosError) {
+            const { status, data } = err.response || { status: 500, data: undefined }
+            res.status(status).send(data)
+          } else {
+            res.status(500)
+          }
+        }
       })
     )
   }
