72c094602ff35593330427a334b6e073f8316238
Samuel Massé <samuelmasse4@gmail.com>
2022-04-04 16:40:02 -0400

    chore(dev): cloud local

--- packages/studio-be/src/index.ts
+++ packages/studio-be/src/index.ts
@@ -83,9 +83,16 @@ try {
 
   process.IS_PRO_AVAILABLE = fs.existsSync(path.resolve(process.PROJECT_LOCATION, 'pro')) || !!process.pkg
   process.BPFS_STORAGE = process.core_env.BPFS_STORAGE || 'disk'
-  process.CLOUD_CONTROLLER_ENDPOINT = process.env.CLOUD_CONTROLLER_ENDPOINT || 'https://controllerapi.botpress.dev'
-  process.CLOUD_OAUTH_ENDPOINT = process.env.CLOUD_OAUTH_ENDPOINT || 'https://oauth.botpress.dev/oauth2/token'
-  process.CLOUD_NLU_ENDPOINT = process.env.CLOUD_NLU_ENDPOINT || 'https://nlu.botpress.dev'
+
+  const localCloud = yn(process.env.CLOUD_LOCAL)
+  process.CLOUD_CONTROLLER_ENDPOINT =
+    process.env.CLOUD_CONTROLLER_ENDPOINT ||
+    (localCloud ? 'http://localhost:3600' : 'https://controllerapi.botpress.dev')
+  process.CLOUD_OAUTH_ENDPOINT =
+    process.env.CLOUD_OAUTH_ENDPOINT ||
+    (localCloud ? 'http://localhost:4444/oauth2/token' : 'https://oauth.botpress.dev/oauth2/token')
+  process.CLOUD_NLU_ENDPOINT =
+    process.env.CLOUD_NLU_ENDPOINT || (localCloud ? 'http://localhost:3200' : 'https://nlu.botpress.dev')
 
   process.CLUSTER_ENABLED = yn(process.env.CLUSTER_ENABLED) || false
   process.IS_PRO_ENABLED = yn(process.env.PRO_ENABLED) || yn(process.env['BP_CONFIG_PRO_ENABLED']) || false
