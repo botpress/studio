f9caba00dc1074620de1a15187da49fd6bb246ad
Samuel Massé <samuelmasse4@gmail.com>
2022-04-22 16:22:40 -0400

    chore: refact tsconfig

--- packages/studio-be/scripts/copy_builtin.js
+++ packages/studio-be/scripts/copy_builtin.js
@@ -4,18 +4,16 @@ const path = require('path')
 const rootDir = path.join(__dirname, '..')
 
 const files = [
-  { source: './src/builtin', dest: './out/builtin' },
-  { source: './src/typings/node.d.txt', dest: './out/typings/node.d.txt' },
-  { source: './src/typings/es6include.txt', dest: './out/typings/es6include.txt' },
-  { source: './src/sdk/botpress.d.ts', dest: './out/sdk/botpress.d.txt' },
-  { source: './src/sdk/botpress.runtime.d.ts', dest: './out/sdk/botpress.runtime.d.txt' }
+  { source: './src/builtin', dest: './dist/builtin' },
+  { source: './src/typings/node.d.txt', dest: './dist/typings/node.d.txt' },
+  { source: './src/typings/es6include.txt', dest: './dist/typings/es6include.txt' },
+  { source: './src/sdk/botpress.d.ts', dest: './dist/sdk/botpress.d.txt' },
+  { source: './src/sdk/botpress.runtime.d.ts', dest: './dist/sdk/botpress.runtime.d.txt' }
 ]
 
 for (const file of files) {
   const source = path.join(rootDir, file.source)
   const target = path.join(rootDir, file.dest)
-
-  console.log(`copying "${source}" to "${target}"`)
 
   fse.copySync(source, target)
 }
