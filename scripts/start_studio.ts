74a8c9ac5a0e95f5c466c9ce7829f3264581d03b
Samuel Massé <samuelmasse4@gmail.com>
2022-04-22 15:36:24 -0400

    chore: refact dev a bit

--- scripts/start_studio.ts
+++
@@ -1,5 +0,0 @@
-console.info(`
-  The studio is not meant to be run as a standalone just yet.
-  Please set DEV_STUDIO_PATH on the main server repository to the out/ folder of packages/studio-be
-  
-  If you absolutely want to run the studio as a standalone, please type 'yarn start' in 'packages/studio-be`)
