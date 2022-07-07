12571529a72cfb2c666cc064d6da3702b1296636
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2022-07-06 22:07:45 -0400

    useReducer instead

--- packages/studio-ui/src/web/components/Layout/StatusBar/TrainingStatus/index.tsx
+++ packages/studio-ui/src/web/components/Layout/StatusBar/TrainingStatus/index.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { NLU } from 'botpress/sdk'
+import { lang } from 'botpress/shared'
 import { Dictionary } from 'lodash'
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useReducer } from 'react'
 import { connect } from 'react-redux'
 import { RootReducer } from '~/reducers'
 import EventBus from '~/util/EventBus'
@@ -19,16 +20,25 @@ interface TrainStatusEvent {
   trainSession: NLU.TrainingSession
 }
 
+const trainStatusReducer = (state, action) => {
+  if (action.type === 'setAllTrainSessions') {
+    return action.data
+  } else if (action.type === 'setTrainSession') {
+    const trainSession: NLU.TrainingSession = action.data.trainSession
+    return { ...state, [trainSession.language]: trainSession }
+  }
+}
+
 const isTrainStatusEvent = (event: any | TrainStatusEvent): event is TrainStatusEvent =>
   event.type === 'nlu' && event.trainSession
 
 const TrainingStatusComponent: FC<Props> = (props: Props) => {
   const { languages } = props
-  const [trainSessions, setTrainSessions] = useState<Dictionary<NLU.TrainingSession>>({})
+  const [trainSessionsState, dispatch] = useReducer(trainStatusReducer, {})
 
   const onStatusBarEvent = (event: any) => {
     if (isTrainStatusEvent(event) && event.botId === window.BOT_ID) {
-      setTrainSessions({ ...trainSessions, [event.trainSession.language]: event.trainSession })
+      dispatch({ type: 'setTrainSession', data: { trainSession: event.trainSession } })
     }
   }
 
@@ -63,7 +73,7 @@ const TrainingStatusComponent: FC<Props> = (props: Props) => {
         sessions.forEach((session) => {
           trainSessionBatchUpdate[session.language] = session
         })
-        setTrainSessions({ ...trainSessions, ...trainSessionBatchUpdate })
+        dispatch({ type: 'setAllTrainSessions', data: trainSessionBatchUpdate })
       })
       .catch((err) => {})
   }, [languages])
@@ -72,9 +82,9 @@ const TrainingStatusComponent: FC<Props> = (props: Props) => {
   if (!languages || languages.length < 1) {
     return null
   } else if (languages.length === 1) {
-    return <SingleLang trainSession={trainSessions[languages[0]]} />
+    return <SingleLang trainSession={trainSessionsState[languages[0]]} trainLabel={lang.tr('statusBar.trainChatbot')} />
   } else {
-    return <MultiLang languages={languages} trainSessions={trainSessions} />
+    return <MultiLang languages={languages} trainSessions={trainSessionsState} />
   }
 }
 
