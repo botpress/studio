bb9810213e32fd7f92aa3782ddd154f74ea8225a
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2022-08-08 15:00:34 -0400

    try..catch

--- packages/studio-ui/src/web/components/Layout/TopNav/cloud/pat.ts
+++ packages/studio-ui/src/web/components/Layout/TopNav/cloud/pat.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
 
 export const fetchPatStatus = async (pat: string, ac: AbortController): Promise<boolean> => {
-  const resp = await axios.get(`${window.CLOUD_CONTROLLER_ENDPOINT}/v1/pat/authenticate`, {
-    headers: { Authorization: `bearer ${pat}` },
-    validateStatus: () => true,
-    signal: ac.signal
-  })
-
-  const authenticated = resp.status === 204 && resp.headers['x-user-id'] !== undefined
-  return authenticated
+  try {
+    await axios.get(`${window.CLOUD_CONTROLLER_ENDPOINT}/v1/pat/authenticate`, {
+      headers: { Authorization: `bearer ${pat}` },
+      signal: ac.signal
+    })
+    return true
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      return false
+    }
+    throw err
+  }
 }
