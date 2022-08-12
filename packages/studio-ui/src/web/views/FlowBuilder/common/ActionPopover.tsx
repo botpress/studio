45cf7a3748f509636d97a10821137af7dd5ff97e
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2022-08-12 09:27:54 -0400

    git rename actionPopover to ActionPopover

---
+++ packages/studio-ui/src/web/views/FlowBuilder/common/ActionPopover.tsx
@@ -0,0 +1,59 @@
+import { H4, Popover, PopoverInteractionKind } from '@blueprintjs/core'
+import { lang } from 'botpress/shared'
+import classnames from 'classnames'
+import { parseActionInstruction } from 'common/action'
+import React, { FC } from 'react'
+
+import style from './style.scss'
+
+interface Props {
+  text: string
+  className: string
+}
+
+export const ActionPopover: FC<Props> = (props) => {
+  const actionInstruction = parseActionInstruction(props.text.trim())
+  const actionName = `${actionInstruction.actionName}`
+
+  let callPreview: string = ''
+  if (actionInstruction.argsStr && actionInstruction.argsStr !== '{}') {
+    try {
+      const parameters = JSON.parse(actionInstruction.argsStr)
+      callPreview = JSON.stringify(parameters, null, 2)
+    } catch (err) {
+      console.error('[ActionPopover] Error parsing instructions:', err)
+      callPreview = lang.tr('studio.flow.node.actionInstructionParsingError', { msg: err.message })
+    }
+  }
+
+  const PopoverContent: JSX.Element = (
+    <div className={style.popoverContent}>
+      <H4>⚡ {actionName}</H4>
+      {!callPreview && <p>{lang.tr('studio.flow.node.actionNoArguments')}</p>}
+      {!!callPreview && (
+        <div>
+          <p>{lang.tr('studio.flow.node.actionArguments')}</p>
+          <pre>{callPreview}</pre>
+        </div>
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      <Popover
+        usePortal
+        boundary="viewport"
+        content={PopoverContent}
+        interactionKind={PopoverInteractionKind.HOVER}
+        minimal
+        position="left"
+      >
+        <div className={classnames(props.className, style.fn, style['action-item'])}>
+          <span className={style.icon}>⚡</span>
+          <span className={style.name}>{actionName}</span>
+        </div>
+      </Popover>
+    </div>
+  )
+}
